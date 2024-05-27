815b5d1046590f0c934dc841f99f26e5d378b9cc
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-05-28 02:51:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_
 
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot\n\nI can send you restricted content by it's post link__**",
+	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot\n\nI can send you restricted content by it's post link__**\n\n Use /help for more",
 	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("Update Channel", url="https://t.me/MODSMAVI")],[ InlineKeyboardButton("Support Group", url="https://t.me/mavibot_support")]]), reply_to_message_id=message.id)
 
 
